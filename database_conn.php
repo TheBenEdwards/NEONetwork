5ab2701bb0f828f55864c4ee1ab40d6fb3c7d488
Ben Edwards <benji.edwards60@gmail.com>
2020-07-18 23:06:19 +0100

    Delete database_conn.php

--- database_conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dbConn = new mysqli('localhost', 'unn_w17004394', 'PASSWORDHERE', 'unn_w17004394'); // make db connection
-
-if ($dbConn->connect_error) {
-    echo "<p>Connection failed: ".$dbConn->connect_error."</p>\n";
-    exit;
-}
-?>
