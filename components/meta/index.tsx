4d3ae6636d8366ea99d95bb22036ec67c3716721
Ben Edwards <benji.edwards60@gmail.com>
2021-06-11 14:26:27 +0100

    image optimisations

--- components/meta/index.tsx
+++ components/meta/index.tsx
@@ -6,7 +6,7 @@ const Meta = () => {
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <meta charSet="utf-8" />
             <link rel="icon" type="icon/ico" href="/static/favicon.ico" />
-            <link rel="icon" type="image/png" href="/static/icon/logo.png" />
+            <link rel="icon" type="image/png" href="/static/images/logo.png" />
             <title>Portal Event Hub</title>
         </>
     )
